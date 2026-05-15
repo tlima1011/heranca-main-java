9ee75d43269964d5e11f821ae05ded5757693b7e
tlima1011 <thiagolima1011@gmail.com>
2026-05-14 22:11:40 -0300

    Criação de Classe Employee Completo

--- src/com/power_employees/entities/Employee.java
+++ src/com/power_employees/entities/Employee.java
@@ -4,6 +4,26 @@ public class Employee {
 	
 	private String Name; 
 	private int Hours; 
-	private double valuePerHour;
+	private double ValuePerHour;
 	
+	public Employee() { 
+		
+	}
+
+	public Employee(String name, int hours, double valuePerHour) {
+		Name = name;
+		Hours = hours;
+		ValuePerHour = valuePerHour;
+	}
+	
+	public double payment() { 
+		return Hours * ValuePerHour; 
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(Name + " - + $ " + String.format("%.2f", payment())+"\n");
+		return sb.toString(); 
+	}
 }
