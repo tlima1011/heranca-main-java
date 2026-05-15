8458e81c6e971f32bba02249bc723880bc7d6e29
tlima1011 <thiagolima1011@gmail.com>
2026-05-14 23:26:12 -0300

    Teste Class and Update

--- src/com/power_employees/entities/Employee.java
+++ src/com/power_employees/entities/Employee.java
@@ -3,15 +3,13 @@ package com.power_employees.entities;
 import java.util.ArrayList;
 
 public class Employee {
-	
-	private String Name; 
-	private int Hours; 
+
+	private String Name;
+	private int Hours;
 	private double ValuePerHour;
-	
-	public ArrayList<Employee> employees = new ArrayList<>(); 
-	
-	public Employee() { 
-		
+
+	public Employee() {
+
 	}
 
 	public Employee(String name, int hours, double valuePerHour) {
@@ -19,19 +17,16 @@ public class Employee {
 		Hours = hours;
 		ValuePerHour = valuePerHour;
 	}
-	
-	public double payment() { 
-		return Hours * ValuePerHour; 
-	}
-	
-	public void addEmployee(Employee e) {
-		employees.add(e);
+
+	public double payment() {
+		return Hours * ValuePerHour;
 	}
 
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(Name + " - + $ " + String.format("%.2f", payment())+"\n");
-		return sb.toString(); 
+	public void getPayments(ArrayList<Employee> list) {
+		System.out.println();
+		System.out.println("PAYMENTS:");
+		for (Employee employee : list) {
+			System.out.println(employee.Name + " - $ " + String.format("%.2f", employee.payment()));
+		}	
 	}
 }
